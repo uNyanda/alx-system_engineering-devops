7f7daf58d3f7063a7980f81487cf13047e756c15
uNyanda <ayabulelamdlalose@outlook.com>
2024-04-25 15:18:23 +0200

    install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # This manifest installs flask from pip3
 
 package { 'flask':
-  ensure  => '2.1.0',
-  command => 'pip3 install flask',
+  ensure   => '2.1.0',
+  command  => 'pip3 install flask',
+  provider => 'pip3',
 }
